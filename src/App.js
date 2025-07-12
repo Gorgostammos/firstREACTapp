d28f61802a1b221afe93370a031f65175ee3f82f
GergesTammo2001 <gorgos2002@icloud.com>
2025-07-13 00:37:25 +0200

    la til noe nytt

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import bil from './sun-svgrepo-com.svg';
+import React, { useState } from 'react'; 
 
 function App() {
+  const [count, setCount] = useState(0); 
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +13,10 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <button className="button" onClick={() => setCount(count + 1)}>
+          <span>Hover </span>
+        </button>
+        <p>Antall trykk: {count}</p> 
         <h1>dette er mitt første React app</h1>
         <p>hei på deg jeg er jojo</p>
         <a
